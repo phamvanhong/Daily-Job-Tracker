34f7d73d8368b2257f36cb6976122684e1ed3267
Van Hong <vanhong542002@gmail.com>
2025-02-14 12:33:31 +0700

    update

--- src/object/kafka_streaming.py
+++ src/object/kafka_streaming.py
@@ -1,30 +1,33 @@
 import requests
 
-class KafkaStreaming:
+class ETL:
     def __init__(self, 
                  url: str,
                  query: dict,
-                 headers: dict):
+                 headers: dict) -> None:
         self.url = url
         self.query = query
         self.headers = headers
     
-    def get_data(self):
+    def extract(self):
         response = requests.get(self.url, 
                                 params=self.query,
                                 headers=self.headers)
         return response.json()['data']
+    
+    def transform(self):
+        pass
 
 
 url = "https://linkedin-data-api.p.rapidapi.com/search-jobs-v2"
 
-querystring = {"locationId":"104195383","datePosted":"anyTime","sort":"mostRelevant"}
+querystring = {"keywords":"data engineer","locationId":"104195383","datePosted":"anyTime","sort":"mostRelevant"}
 
 headers = {
 	"x-rapidapi-key": "3e91c0dc89mshc23aeb55eb34c6dp1dd327jsndcd4a975b727",
 	"x-rapidapi-host": "linkedin-data-api.p.rapidapi.com"
 }
 
-kafka_streaming = KafkaStreaming(url, querystring, headers)
-data = kafka_streaming.get_data()
+kafka_streaming = ETL(url, querystring, headers)
+data = kafka_streaming.extract()
 print(data)
